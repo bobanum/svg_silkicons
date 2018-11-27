9a5699bfd3d190d0ffc5e8620460b809a58adbf9
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2018-11-27 18:12:48 -0500

    First commit

---
+++ _ini.php
@@ -0,0 +1,29 @@
+<?php
+// Projet : silkicons
+$ini = array (
+	//Le titre qui s'affiche dans la liste
+	'titre' => 'silkicons',
+
+	//Categories sous forme "Catégorie/Sous-Catégorie/..."
+	'categorie' => 'Quarantaine',
+
+	//Un préfixe à mettre devant le titre pour le tri "Cours 01 : "
+	'prefixe' => '',
+
+	//Un tableau de liens (étiquette=>url) ou une série de lignes (étiquette=url)
+	'liens' => array (
+),
+
+	//Booléen. Doit-on afficher la source?
+	'source' => false,
+
+	//Booléen. Le dossier est-il visible dans la liste? Il reste tout de même accessible.
+	'visible' => false,
+
+	//La date d'ajout du projet
+	'dateAjout' => '2013-03-06',
+
+	//Une description plus ou moins longue du projet
+	'description' => '',
+);
+?>
